dc93f9bb46b874fa0b2f006fad74a30900a37b25
Jayveer-code <jayuchavda444@gmail.com>
2024-05-04 16:46:22 -0700

    Add Beginning Compplete

--- sll.py
+++ sll.py
@@ -6,19 +6,20 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        # Step 1: Initialize LinkedList with head node as None
+        # Step 2: Initialize LinkedList with head node as None
         self.head = None
 
     def is_empty(self):
-        # Step 1: Check if the LinkedList is empty
+        # Step 3: (Condition no 1) Check if the LinkedList is empty
         return self.head is None
 
     def print_list(self):
-        # Step 3: Print the elements of the LinkedList
+        # Step 4: (Condition no 2) Print the elements of the LinkedList
         if self.is_empty():
             print("Linked List is Empty")
         else:
-            new_node = self.head
+            # Step 5: (Condition no 3) If it is Not None that what to do
+            new_node = self.head  
             while new_node:
                 print(new_node.data)
                 new_node = new_node.next
